24cdbbae49fac002f55c0584126c59c4518f45be
shreeshailjalageri <smjalageri400@gmail.com>
2023-06-03 11:35:18 +0530

    first eclipse commit

---
+++ src/main/java/calulator/Calculator.java
@@ -0,0 +1,20 @@
+package calulator;
+
+public class Calculator {
+
+	public void add() {
+		int a=10;
+		int b=15;
+		System.out.println("addition of 2 numbers is"+ (a+b));
+	}
+public void sub() {
+	int a=23,
+	b=15;
+	System.out.println("subtraction of 2 numbers is"+ (a-b));
+	}
+public void div() {
+	int a=10;
+	int b=15;
+	System.out.println("div of 2 numbers is"+ (a/b));
+}
+}
